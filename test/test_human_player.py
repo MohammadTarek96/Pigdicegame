21bcc6a670223bc422473701190fedd3e6d91ec5
MohammadTarek96 <capjoh@gmail.com>
2022-03-10 18:13:13 +0000

    Add tests for human player

---
+++ test/test_human_player.py
@@ -0,0 +1,24 @@
+
+
+import unittest
+from pig.core.player import HumanPlayer
+
+class TestHumanPlayer(unittest.TestCase):
+
+    def test_initialization(self):
+        
+        name = "john dice"
+        player = HumanPlayer(name)
+        
+        self.assertIsInstance(player, HumanPlayer)
+        self.assertEqual(name, player.name)
+
+    def test_change_name(self):
+        name = "john dice"
+        player = HumanPlayer(name)
+
+        new_name = "james dice"
+        player.change_name(new_name)
+
+        self.assertEqual(new_name, player.name)
+
