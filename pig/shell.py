69f8774c40f85a7655d17cafab45406cc8060c1e
MohammadTarek96 <capjoh@gmail.com>
2022-03-11 11:31:08 +0000

    implement the shell for the main game loop

--- pig/shell.py
+++ pig/shell.py
@@ -13,13 +13,12 @@ from core.game import Game
 from core.player import HumanPlayer
 from core.exceptions import TooManyPlayersError
 
-
 class MainMenuShell(cmd.Cmd):
     """
     Greets the player, and guides him through to starting the game
     """
     intro = "Welcome to the game! Type help or ? to list commands. \n"
-    prompt = "(PigGame)> "
+    prompt = "(PigGameMenu)> "
     game = None
     player_name = None
     player_instance = None
@@ -37,8 +36,9 @@ class MainMenuShell(cmd.Cmd):
     def do_start(self, _):
         """Starts the game, and launches the game shell"""
         msg = "Your game will now start. Good luck!"
-
-        return True
+        quit_helper = [False] # passing a list will be a pass-by-reference, and any modifications done will be usable here
+        game_shell = GameShell(self.game, quit_helper).cmdloop()
+        return quit_helper[0]
 
     def do_add(self, _):
         """Adds a new bot player to the game. Some say the game is more fun with more players!"""
@@ -82,21 +82,69 @@ class GameShell(cmd.Cmd):
     """
     Handles the input while in-game.
     Commands available:
+    ! end-restart
     - end/q/quit/EOF
-    - roll
-    - end_turn/pass
     - restart
 
-    ! immoral, but
-    - CHEAT
+    ! game commands
+    - roll
+    - end turn/pass
+
+    ! immoral but
+    - roll [a loaded dice {points}]
     """
     intro = "Your turn. Type ? or help to list all the commands"
-    prompt = "(PigGame> )"
+    prompt = "(PigGame)>"
     game = None
 
-    def __init__(self, game):
+    def __init__(self, game, quit_helper):
         """Initialize the menu"""
-        super.__init__()
+        super().__init__()
         self.game = game
+        self.quit_helper = quit_helper
+    
+    # game handlers
+    def do_roll(self, _):
+        self.game.roll_for_current_player()
+
+    def do_end(self, arg):
+        """ End [arg].
+            - g/game: ends the game. Alias for exit
+            - t/turn: ends the turn. Alias for pass
+        """
+        if arg == "":
+            print("end requires an additional argument.Type '? end' for help")
+            return False
+        elif arg.lower() in ['t', 'turn']:
+            self.do_pass("")
+        elif arg.lower() in ['g', 'game']:
+            self.do_exit("")
+        else:
+            print("argument not recognized. Type '? end' for a list of possible arguments")
+
+    def do_pass(self, _):
+        self.game.end_turn_for_current_player()
 
 
+    # game commands 
+    def do_restart(self,_):
+        """Return to the main menu, and lets you configure a new game"""
+        return True
+
+
+    def do_q(self, _):
+        """Ends the game, alias for exit"""
+        return self.do_exit(_)
+
+    def do_quit(self, _):
+        """Ends the game, alias for exit"""
+        return self.do_exit(_)
+
+    def do_EOF(self, _):
+        """Ends the game, when there is no more input"""
+        return self.do_exit(_)
+
+    def do_exit(self, _):
+        """Ends the game, alias for exit"""
+        self.quit_helper[0] = True
+        return True
